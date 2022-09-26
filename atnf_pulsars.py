537c0110c74a9a025465699f793a4455ac702d17
SevenSpheres <56284207+SevenSpheres@users.noreply.github.com>
2022-09-25 20:10:44 -0600

    Update to 1.68

--- atnf_pulsars.py
+++ atnf_pulsars.py
@@ -26,7 +26,7 @@ datafile = open('atnf_pulsars.csv', 'r')
 stcfile = open('atnf_pulsars.stc', 'w')
 sscfile = open('pulsar_jets.ssc', 'w')
 script = open('mark_pulsars_period.cel', 'w')
-stcfile.write("""# Catalog of 3320 pulsars for Celestia from the ATNF database, v1.67.
+stcfile.write("""# Catalog of 3341 pulsars for Celestia from the ATNF database, v1.68.
 # Source: http://www.atnf.csiro.au/research/pulsar/psrcat/
 # Manchester, R. N., Hobbs, G. B., Teoh, A. & Hobbs, M., AJ, 129, 1993-2006 (2005)
 #
